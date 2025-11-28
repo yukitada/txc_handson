4cb94288c1347619fc2bf86d1a2fc7ae5a98bd04
yukitada <ytada@jp.ibm.com>
2025-11-29 01:08:18 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -158,3 +158,10 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// 環境変数を返す
+app.get("/env", (req, res) => {
+  res.json(process.env);
+});
+
+
