fe57b19e9a82e8c169a4468c659a5fddb4a253fa
yukitada <ytada@jp.ibm.com>
2025-11-29 03:12:27 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post("/chat", async (req, res) => {
       body: '{"messages":[{"role":"assistant","content":userInput}]'
     });
 
-    const data = await response.json();
+    const data = await response.text();
 
     const reply = data
 
